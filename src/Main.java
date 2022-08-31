5c440b289c5782bf79e32abe8dadc1be6c3054ba
Aleksandr Desyatnichenko <alik_kenig@rambler.ru>
2022-08-31 18:42:17 +0200

    Solved the second task

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Locale;
 import java.util.Scanner;
 
 public class Main {
@@ -14,6 +15,7 @@ public class Main {
             System.out.println("1. Добавить покупку.");
             System.out.println("2. Показать покупки.");
             System.out.println("3. Удалить покупку.");
+            System.out.println("4. Поиск по покупкам.");
 
             String input = scan.nextLine();
 
@@ -35,7 +37,7 @@ public class Main {
                 }
                 case 3: {
                     shopList(shopCart);
-                    System.out.println("\nКакую покупку хотите удалить? Введите номер или название.");
+                    System.out.println("Какую покупку хотите удалить? Введите номер или название.");
                     input = scan.nextLine();
 
                     int index;
@@ -50,6 +52,19 @@ public class Main {
                     shopCart.remove(index - 1);
                     System.out.println("Покупка \"" + remPurchase + "\" удалена.");
                     shopList(shopCart);
+                    break;
+                }
+                case 4: {
+                    System.out.println("\nВведите текст для поиска:");
+                    input = scan.nextLine().toLowerCase();
+
+                    System.out.println("\nНайдено:");
+                    for (int i = 0; i < shopCart.size(); i++) {
+                        if (shopCart.get(i).toLowerCase().contains(input)) {
+                            System.out.println((i + 1) + ". " + shopCart.get(i));
+                        }
+                    }
+                    System.out.println();
                     break;
                 }
                 default:
